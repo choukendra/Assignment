5a371b06733d44e7d9358975fe68ce79caad2c64
choukendra <55104616+choukendra@users.noreply.github.com>
2020-10-20 13:15:58 -0700

    Add files via upload

--- reviewAssignment1/question1.java
+++ reviewAssignment1/question1.java
@@ -4,10 +4,10 @@ class question1 {
         int x = 0;
         int t = 0;
         //2. get user input
-        for(int n = 1; n <= 999; n++){
-            while(n%5 == 0|| n%3 == 0){   
+        for(int n = 1; n < 1000; n++){
+            if(n%5 == 0|| n%3 == 0){   
             t = t + n;
-            n++;
+           
         }
         }
         System.out.println("Answer: " + t);
